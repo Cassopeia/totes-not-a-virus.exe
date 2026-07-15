b371c28a4948d2122e8319debfc45594d68052a1
Cassopeia <acol33310@gmail.com>
2026-07-15 18:39:19 -0500

    Update terminal.js

--- js/terminal.js
+++ js/terminal.js
@@ -1,1 +1,38 @@
+const input = document.getElementById("nameInput");
+const response = document.getElementById("response");
 
+
+input.addEventListener("keydown", function(event){
+
+    if(event.key === "Enter"){
+
+
+        let name = input.value.toLowerCase();
+
+
+        if(name === "ale" || name === "alessandro"){
+
+
+            response.innerHTML =
+            "ACCESS GRANTED.";
+
+
+            input.style.display = "none";
+
+
+        }
+
+        else {
+
+
+            response.innerHTML =
+            "NICE TRY.";
+
+
+            input.value = "";
+
+        }
+
+    }
+
+});
